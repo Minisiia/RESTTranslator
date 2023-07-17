9db5839b846f8650ff9564c47c1570516977f7a9
Minisiia <ar_tik@yahoo.com>
2023-07-17 21:39:50 +0300

    6. Web service. 4. rest & soap

--- src/main/java/entity/Translations.java
+++ src/main/java/entity/Translations.java
@@ -1,4 +1,4 @@
-package accessingdatamysql;
+package entity;
 
 import javax.persistence.*;
 
--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,6 +1,6 @@
 package util;
 
-import accessingdatamysql.Translations;
+import entity.Translations;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
